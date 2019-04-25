ec322b08acda4a818d3907ef313ebfcd6a88796d
Sujanitha Mahalingam <48694682+Sujanitha@users.noreply.github.com>
2019-04-25 06:24:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// $('#circle').click(function(){
+//     $('img').attr('src','https://i.pinimg.com/originals/f5/7e/00/f57e00306f3183cc39fa919fec41418b.jpg
+https://i.pinimg.com/originals/f5/7e/00/f57e00306f3183cc39fa919fec41418b.jpg
+// ')
+
+// })
+
+// $('#circle').click(function(){
+//     alert('clicked!');
+// });
+
+// $('#circle').hover(function(){
+//    $('p').html('we cahnged the text');
+// });
+
+
+// 
+
+
+ $('.square').click(function(){
+     $('.square').css('background-color','black')
+    
+    });
+
+
+    // $('.square').click(function(){
+    //     $('this').css('background-color','black')
+        
+    //     });
